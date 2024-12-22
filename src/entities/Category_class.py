5300d65dbbae067c23794fb02f5fcbbfbdc3529f
farr13 <144727009+farr13@users.noreply.github.com>
2024-12-22 17:33:54 +0000

    Basic skeleton of program folders alognside entity prototypes

---
+++ src/entities/Category_class.py
@@ -0,0 +1,12 @@
+class Category:
+
+    def __init__(self, name, money_allocated):
+        self.name = name
+        self.money_allocated = money_allocated 
+    
+    def change_allocation(self, amount):
+        """ Changes allocation of money for the specified category
+
+         amount: money subtracted from category total
+        """
+        self.money_allocated - amount
---
+++ src/entities/History_class.py
@@ -0,0 +1,8 @@
+class History:
+
+    def __init__(self, amount_spent, description, category_name, date):
+        self.amount_spent = amount_spent
+        self.description = description
+        self.category_name = category_name
+        self.date = date
+    
